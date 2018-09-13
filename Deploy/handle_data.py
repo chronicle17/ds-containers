0b2580d71c20edd7d38027289e4e66fea13d3423
chronicle17 <pcoachman@hotmail.com>
2018-09-13 06:38:00 -0400

    Updated for flight delay prediction

--- Deploy/handle_data.py
+++ Deploy/handle_data.py
@@ -1,19 +1,26 @@
 # handle_data.py
 import json
 import numpy as np
+import hashlib
+
+def hashme(s):
+    return int(hashlib.sha1(s).hexdigest(), 16) % (10 ** 8)
 
 def invoke_predict(model, raw_json):
     # Decode JSON string, store as dict
     input_dict = json.loads(raw_json)#.decode())
 
     # Ordered feature list
-    feature_list = ['sepal_len', 'sepal_width', 'petal_len', 'petal_width']
-
+    
+	
     # Initialize empty list for array of inputs
     mapped_inputs = []
 
     for feature in feature_list:
         mapped_inputs.append(input_dict[feature])#[0])
+
+    for hashfeature in hashfeature_list:
+        mapped_inputs.append(hashme(input_dict[hashfeature]))#[0])
 		
     x_to_predict = np.asarray(mapped_inputs, dtype=np.float64)
     x_to_predict = x_to_predict.reshape(1, -1)
